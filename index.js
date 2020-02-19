8ac7c79861e01270e90ee88ee6314dea8254ee42
ksk94966 <skp.kurapati@gmail.com>
2020-02-18 21:25:25 -0600

    added country name as well

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ $(document).ready(function(){
                    var path = d.path;
                    var title= d.title;
                    var city = d.city;
-                   var date =  d.taken;            
-                   item = "<li><img class=\"sqimage\" src=\"images/square/"+path+"\" alt=\""+title+"\" ipath=\""+path+"\" icity=\""+city+"\" idate=\""+date+"\" ititle=\""+title+"\"></li>"
+                   var date =  d.taken;
+                   var country = d.country;            
+                   item = "<li><img class=\"sqimage\" src=\"images/square/"+path+"\" alt=\""+title+"\" ipath=\""+path+"\" icity=\""+city+"\" icountry=\""+country+"\" idate=\""+date+"\" ititle=\""+title+"\"></li>"
                    $(".gallery").append(item);
                    });
             },
@@ -28,7 +29,8 @@ $(document).ready(function(){
        var city =  $(this).attr("icity");
        var date = $(this).attr("idate");
        var path = $(this).attr("ipath");
-      var ditem = "<div class=\"mdimage\" id=\"preview\" title=\""+title+"\"><img src=\"images/medium/"+path+"\" alt=\""+title+"\"><p>"+title+","+city+","+date+"</p></div>"
+       var country = $(this).attr("icountry");
+      var ditem = "<div class=\"mdimage\" id=\"preview\" title=\""+title+"\"><img src=\"images/medium/"+path+"\" alt=\""+title+"\"><p>"+title+"<br><i>"+city+", "+country+"</i> ["+date+"]</p></div>"
         $("body").append(ditem);  
       });
 
@@ -45,11 +47,10 @@ $(document).ready(function(){
         })
       });
 
-
       $(document).on("mousemove",function(event)
       { 
-        var x = 6 + event.pageX;
-        var y = 6 + event.pageY;
+        var x = 4 + event.pageX;
+        var y = 4 + event.pageY;
         $("#preview").css("left",x);
         $("#preview").css("top",y);
       });
